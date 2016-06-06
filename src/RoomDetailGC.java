c43fb252a89eabab894880f3e7c94573fa75c374
Jonathan Abdo <jabdo79@gmail.com>
2016-06-06 15:00:43 -0400

    commit lab 1

---
+++ src/RoomDetailGC.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class RoomDetailGC {
+
+	public static void main(String[] args) {
+		//initialize scanner and variables that will be used
+		Scanner sc = new Scanner(System.in);
+		double area=0, perimeter=0, volume=0, length=0, width=0, height=0;
+		boolean cont = true;
+		
+		System.out.println("Welcome to Grand Circus' Room Detail Generator!\n");
+		
+		while (cont) {
+			//take user inputs
+			System.out.print("Enter length: ");
+			length = sc.nextDouble();
+			System.out.print("Enter Width: ");
+			width = sc.nextDouble();
+			System.out.print("Enter Height: ");
+			height = sc.nextDouble();
+			//calculate are, perimeter and volume
+			area = length * width;
+			System.out.println("Area: " + area);
+			perimeter = 2 * length + 2 * width;
+			System.out.println("Perimeter: " + perimeter);
+			volume = length*width*height;
+			System.out.println("Volume: "+volume);
+			//ask user to continue
+			System.out.print("\nContinue?(y/n)");
+			if(sc.next().charAt(0)!='y'){
+				cont = false;
+			}
+			System.out.println("Goodbye!");
+		}		
+		
+		sc.close();
+	}
+
+}
