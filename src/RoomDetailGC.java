54263f1586971adac31c97bd533b4c2de7a66a35
Jonathan Abdo <jabdo79@gmail.com>
2016-06-06 16:05:09 -0400

    finish input validation

--- src/RoomDetailGC.java
+++ src/RoomDetailGC.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class RoomDetailGC {
+	
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		//initialize scanner and variables that will be used
-		Scanner sc = new Scanner(System.in);
 		double area=0, perimeter=0, volume=0, length=0, width=0, height=0;
 		boolean cont = true;
 		
@@ -13,11 +14,11 @@ public class RoomDetailGC {
 		while (cont) {
 			//take user inputs
 			System.out.print("Enter length: ");
-			length = Double.parseDouble(sc.next());
+			length = getDouble();
 			System.out.print("Enter Width: ");
-			width = Double.parseDouble(sc.next());
+			width = getDouble();
 			System.out.print("Enter Height: ");
-			height = Double.parseDouble(sc.next());
+			height = getDouble();
 			//calculate are, perimeter and volume
 			area = length * width;
 			System.out.println("Area: " + area);
@@ -29,10 +30,24 @@ public class RoomDetailGC {
 			System.out.print("\nContinue?(y/n)");
 			if(sc.next().charAt(0)!='y'){
 				cont = false;
+				System.out.println("\nGoodbye!");
 			}
-			System.out.println("\nGoodbye!");
-		}		
+		}	
 		//clean up! close the scanner!
 		sc.close();
 	}
+	
+	//validate input as a double and return a double
+	public static double getDouble(){
+		double correctInput=0;
+		
+		while(!sc.hasNextDouble()){
+			System.out.println("Please enter a number.");
+			sc.next();
+		}
+		correctInput = sc.nextDouble();
+		
+		return correctInput;
+	}
+	
 }
